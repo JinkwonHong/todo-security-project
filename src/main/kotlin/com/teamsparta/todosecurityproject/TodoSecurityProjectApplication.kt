bde057d1aa2501b87a99bc583504de34f5f134a6
hongjinkwon <hjk0472@gmail.com>
2024-06-06 16:46:08 +0900

    :tada: 프로젝트 시작

---
+++ src/main/kotlin/com/teamsparta/todosecurityproject/TodoSecurityProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.todosecurityproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoSecurityProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoSecurityProjectApplication>(*args)
+}
