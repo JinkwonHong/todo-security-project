328e8820801f8e4a74630144d23243ad860b5891
hongjinkwon <hjk0472@gmail.com>
2024-06-10 09:43:15 +0900

    :sparkles: Configure the Environment for Applying Query DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
-    kotlin("plugin.noarg") version "1.8.22"
+    kotlin("plugin.noarg") version "1.9.23"
+    kotlin("plugin.allopen") version "1.9.23"
+    kotlin("kapt") version "1.9.23"
 }
 
 noArg {
@@ -19,6 +21,8 @@ allOpen {
     annotation("jakarta.persistence.MappedSuperclass")
     annotation("jakarta.persistence.Embeddable")
 }
+
+val queryDslVersion = "5.0.0"
 
 group = "com.teamsparta"
 version = "0.0.1-SNAPSHOT"
@@ -46,6 +50,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
+    kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
