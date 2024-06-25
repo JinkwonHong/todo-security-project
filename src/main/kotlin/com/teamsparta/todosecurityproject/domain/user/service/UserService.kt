b976fd9092f02e155201e1b5baf8082f44fd1796
hongjinkwon <hjk0472@gmail.com>
2024-06-25 15:02:13 +0900

    :hammer: Organizing code related to User SignIn

--- src/main/kotlin/com/teamsparta/todosecurityproject/domain/user/service/UserService.kt
+++ src/main/kotlin/com/teamsparta/todosecurityproject/domain/user/service/UserService.kt
@@ -16,16 +16,13 @@ class UserService(
     val passwordEncoder: PasswordEncoder,
     val jwtPlugin: JwtPlugin
 ) {
-    @Transactional
-    fun signIn(loginRequest: LoginRequest): LoginResponse {
-        val user = userRepository.findByEmail(loginRequest.email) ?: throw ModelNotFoundException("User", null)
+    fun signIn(request: LoginRequest): LoginResponse {
+        val (email, password) = request
+        val user = userRepository.findByEmail(email)
+            ?.takeIf { passwordEncoder.matches(password, it.password) }
+            ?: throw IllegalArgumentException("Please check your email and password")
 
-        return LoginResponse(
-            accessToken = jwtPlugin.generateAccessToken(
-                subject = user.id.toString(),
-                email = user.email
-            )
-        )
+        return LoginResponse(accessToken = jwtPlugin.generateAccessToken(user.id.toString(), user.email))
     }
 
     @Transactional
