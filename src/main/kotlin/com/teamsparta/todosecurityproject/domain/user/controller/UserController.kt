bdba10320e9991b7c8e48f32ceb619fbaf4278ab
hongjinkwon <hjk0472@gmail.com>
2024-06-25 12:38:44 +0900

    :bug: Change SignIn, SignUp HttpStatus OK -> CREATED

--- src/main/kotlin/com/teamsparta/todosecurityproject/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/teamsparta/todosecurityproject/domain/user/controller/UserController.kt
@@ -2,8 +2,10 @@ package com.teamsparta.todosecurityproject.domain.user.controller
 
 import com.teamsparta.todosecurityproject.domain.user.dto.*
 import com.teamsparta.todosecurityproject.domain.user.service.UserService
+import com.teamsparta.todosecurityproject.infra.security.UserPrincipal
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,23 +15,25 @@ class UserController(
     @PostMapping("/login")
     fun signIn(@RequestBody loginRequest: LoginRequest): ResponseEntity<LoginResponse> {
         return ResponseEntity
-            .status(HttpStatus.OK)
+            .status(HttpStatus.CREATED)
             .body(userService.signIn(loginRequest))
     }
 
     @PostMapping("/signup")
     fun signUp(@RequestBody signUpRequest: SignUpRequest): ResponseEntity<UserResponse> {
         return ResponseEntity
-            .status(HttpStatus.OK)
+            .status(HttpStatus.CREATED)
             .body(userService.signUp(signUpRequest))
     }
 
     @PostMapping("/users/{userId}/profile")
-    fun updateUserProfile (@PathVariable userId: Long,
-                           @RequestBody updateUserProfileRequest: UpdateUserProfileRequest
+    fun updateUserProfile(
+        @AuthenticationPrincipal principal: UserPrincipal,
+        @PathVariable userId: Long,
+        @RequestBody updateUserProfileRequest: UpdateUserProfileRequest
     ): ResponseEntity<UserResponse> {
         return ResponseEntity
             .status(HttpStatus.OK)
-            .body(userService.updateUserProfile(userId, updateUserProfileRequest))
+            .body(userService.updateUserProfile(principal.id, updateUserProfileRequest))
     }
 }
--- src/main/kotlin/com/teamsparta/todosecurityproject/domain/user/dto/LoginRequest.kt
+++ src/main/kotlin/com/teamsparta/todosecurityproject/domain/user/dto/LoginRequest.kt
@@ -1,6 +1,5 @@
 package com.teamsparta.todosecurityproject.domain.user.dto
 
-
 data class LoginRequest(
     val email: String,
     val password: String,
