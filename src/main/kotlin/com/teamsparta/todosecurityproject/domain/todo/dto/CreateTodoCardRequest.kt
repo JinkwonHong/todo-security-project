9a2f46efafe00bbe1fd973730c2c1df02346161e
hongjinkwon <hjk0472@gmail.com>
2024-06-26 10:55:25 +0900

    :hammer: Add Category and Completed Column

--- src/main/kotlin/com/teamsparta/todosecurityproject/domain/todo/dto/CreateTodoCardRequest.kt
+++ src/main/kotlin/com/teamsparta/todosecurityproject/domain/todo/dto/CreateTodoCardRequest.kt
@@ -2,5 +2,6 @@ package com.teamsparta.todosecurityproject.domain.todo.dto
 
 data class CreateTodoCardRequest(
     val title: String,
-    val description: String
+    val description: String,
+    val category: String
 )
---
+++ src/main/kotlin/com/teamsparta/todosecurityproject/domain/todo/model/Category.kt
@@ -0,0 +1,5 @@
+package com.teamsparta.todosecurityproject.domain.todo.model
+
+enum class Category {
+    WORK, STUDY, EXERCISE, PROMISE, OTHER
+}
