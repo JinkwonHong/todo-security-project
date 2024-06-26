ebfcd76607e0e23a6203233552c4191baf993b9f
hongjinkwon <hjk0472@gmail.com>
2024-06-26 12:18:43 +0900

    :bug: Before Writing QueryDsl Code, Modify Response Dto

--- src/main/kotlin/com/teamsparta/todosecurityproject/domain/todo/dto/TodoCardResponseWithComments.kt
+++ src/main/kotlin/com/teamsparta/todosecurityproject/domain/todo/dto/TodoCardResponseWithComments.kt
@@ -1,6 +1,7 @@
 package com.teamsparta.todosecurityproject.domain.todo.dto
 
 import com.teamsparta.todosecurityproject.domain.todo.comment.dto.CommentResponse
+import com.teamsparta.todosecurityproject.domain.todo.model.Category
 import com.teamsparta.todosecurityproject.domain.todo.model.TodoCard
 import com.teamsparta.todosecurityproject.domain.user.dto.UserResponse
 import java.time.ZonedDateTime
@@ -10,6 +11,8 @@ data class TodoCardResponseWithComments(
     val title: String,
     val description: String,
     val user: UserResponse,
+    val category: Category,
+    val completed: Boolean,
     val createdAt: ZonedDateTime?,
     val updatedAt: ZonedDateTime?,
     val comments: List<CommentResponse>
@@ -20,6 +23,8 @@ data class TodoCardResponseWithComments(
                 title = todoCard.title,
                 description = todoCard.description,
                 user = UserResponse.from(todoCard.user),
+                category = todoCard.category,
+                completed = todoCard.completed,
                 createdAt = todoCard.createdAt,
                 updatedAt = todoCard.updatedAt,
                 comments = todoCard.comments.map { CommentResponse.from(it) }
