1a73b92d6e7dd8fecdb8198aebed7c54d307b003
hongjinkwon <hjk0472@gmail.com>
2024-06-27 20:51:31 +0900

    :white_check_mark: Update Pass TestCode Method "Search, Category, Completed 필터 모두 입력하지 않았을 때, Created Desc 으로 정렬되어 조회되는지 확인"

--- src/test/kotlin/com/teamsparta/todosecurityproject/domain/todo/repository/TodoRepositoryTest.kt
+++ src/test/kotlin/com/teamsparta/todosecurityproject/domain/todo/repository/TodoRepositoryTest.kt
@@ -1,31 +1,51 @@
 package com.teamsparta.todosecurityproject.domain.todo.repository
 
 import com.teamsparta.todosecurityproject.domain.todo.comment.model.Comment
+import com.teamsparta.todosecurityproject.domain.todo.comment.repository.CommentRepository
 import com.teamsparta.todosecurityproject.domain.todo.model.Category
 import com.teamsparta.todosecurityproject.domain.todo.model.TodoCard
 import com.teamsparta.todosecurityproject.domain.user.model.User
+import com.teamsparta.todosecurityproject.domain.user.repository.UserRepository
+import com.teamsparta.todosecurityproject.infra.japauditing.JpaAuditingConfig
+import com.teamsparta.todosecurityproject.infra.querydsl.QueryDslConfig
 import com.teamsparta.todosecurityproject.infra.querydsl.QueryDslSupport
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.ActiveProfiles
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import(value = [QueryDslSupport::class])
+@Import(value = [QueryDslConfig::class, JpaAuditingConfig::class])
 @ActiveProfiles("test")
 class TodoRepositoryTest @Autowired constructor(
-private val todosRepository: TodoRepository
+    private val todoRepository: TodoRepository,
+    private val userRepository: UserRepository
 ) {
     @Test
-    fun `Search, Category, Completed 필터 모두 입력하지 않았을 때, Created Desc 으로 정렬되어 조회되는지`() {
+    fun `Search, Category, Completed 필터 모두 입력하지 않았을 때, Created Desc 으로 정렬되어 조회되는지 확인`() {
         // GIVEN
+        userRepository.saveAllAndFlush(DEFAULT_USER_LIST)
+        todoRepository.saveAllAndFlush(DEFAULT_TODOCARD_LIST)
 
         // WHEN
+        val pageable = PageRequest.ofSize(10)
+        val result = todoRepository.findAllWithFilters(
+            keyword = null, category = null, completed = null, sort = "createdAt", pageable = pageable
+        )
 
         // THEN
+        result.shouldNotBeEmpty()
+        result.content.size.shouldBe(10)
+
+        val sortedList = DEFAULT_TODOCARD_LIST.sortedByDescending { it.createdAt }
+        result.content[0].title.shouldBe(sortedList[0].title)
     }
 
     @Test
@@ -72,31 +92,35 @@ private val todosRepository: TodoRepository
 
         // THEN
     }
+
     companion object {
-        private val DEFAULT_USER1 = User(email = "user1@naver.com", password = "password1", nickname = "user1")
-        private val DEFAULT_USER2 = User(email = "user2@gmail.com", password = "password2", nickname = "user2")
+        private val DEFAULT_USER_LIST = listOf(
+            User(email = "user1@naver.com", password = "password1", nickname = "user1"),
+            User(email = "user2@gmail.com", password = "password2", nickname = "user2")
+        )
 
         private val DEFAULT_TODOCARD_LIST = listOf(
-            TodoCard(title = "06/24 오늘의 헬스 진행 계획", description = "벤치프레스 5세트, 덤벨프레스 5세트", user = DEFAULT_USER1, comments = mutableListOf(), completed = false, category = Category.EXERCISE),
-            TodoCard(title = "06/24 공부 계획", description = "알고리즘 문제 풀기, 데이터베이스 복습", user = DEFAULT_USER2, comments = mutableListOf(), completed = false, category = Category.STUDY),
-            TodoCard(title = "06/24 직장 업무", description = "주간 보고서 작성, 회의 준비", user = DEFAULT_USER1, comments = mutableListOf(), completed = true, category = Category.WORK),
-            TodoCard(title = "06/24 친구와의 약속", description = "저녁 식사, 영화 보기", user = DEFAULT_USER2, comments = mutableListOf(), completed = false, category = Category.PROMISE),
-            TodoCard(title = "06/25 기타 할 일", description = "은행 업무, 쇼핑", user = DEFAULT_USER1, comments = mutableListOf(), completed = true, category = Category.OTHER),
-            TodoCard(title = "06/26 운동 계획", description = "스쿼트 5세트, 데드리프트 5세트", user = DEFAULT_USER2, comments = mutableListOf(), completed = false, category = Category.EXERCISE),
-            TodoCard(title = "06/26 공부 계획", description = "자바 프로그래밍 복습, 코딩 테스트 준비", user = DEFAULT_USER1, comments = mutableListOf(), completed = false, category = Category.STUDY),
-            TodoCard(title = "07/01 업무 계획", description = "프로젝트 미팅, 코드 리뷰", user = DEFAULT_USER2, comments = mutableListOf(), completed = true, category = Category.WORK),
-            TodoCard(title = "07/01 약속", description = "오랜만의 친구와 저녁", user = DEFAULT_USER1, comments = mutableListOf(), completed = false, category = Category.PROMISE),
-            TodoCard(title = "07/01 기타", description = "집 청소, 정리 정돈", user = DEFAULT_USER2, comments = mutableListOf(), completed = true, category = Category.OTHER),
-            TodoCard(title = "07/02 운동", description = "유산소 운동, 스트레칭", user = DEFAULT_USER1, comments = mutableListOf(), completed = false, category = Category.EXERCISE),
-            TodoCard(title = "07/02 학습", description = "데이터 사이언스 공부, 통계 복습", user = DEFAULT_USER2, comments = mutableListOf(), completed = false, category = Category.STUDY),
-            TodoCard(title = "07/03 직장 업무", description = "클라이언트 회의, 코드 배포", user = DEFAULT_USER1, comments = mutableListOf(), completed = true, category = Category.WORK),
-            TodoCard(title = "07/03 친구와의 약속", description = "카페에서 대화, 산책", user = DEFAULT_USER2, comments = mutableListOf(), completed = false, category = Category.PROMISE),
-            TodoCard(title = "07/07 기타 할 일", description = "서류 정리, 물건 정리", user = DEFAULT_USER1, comments = mutableListOf(), completed = true, category = Category.OTHER)
+            TodoCard(title = "06/24 오늘의 헬스 진행 계획", description = "벤치프레스 5세트, 덤벨프레스 5세트", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = false, category = Category.EXERCISE),
+            TodoCard(title = "06/24 공부 계획", description = "알고리즘 문제 풀기, 데이터베이스 복습", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = false, category = Category.STUDY),
+            TodoCard(title = "06/24 직장 업무", description = "주간 보고서 작성, 회의 준비", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = true, category = Category.WORK),
+            TodoCard(title = "06/24 친구와의 약속", description = "저녁 식사, 영화 보기", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = false, category = Category.PROMISE),
+            TodoCard(title = "06/25 기타 할 일", description = "은행 업무, 쇼핑", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = true, category = Category.OTHER),
+            TodoCard(title = "06/26 운동 계획", description = "스쿼트 5세트, 데드리프트 5세트", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = false, category = Category.EXERCISE),
+            TodoCard(title = "06/26 공부 계획", description = "자바 프로그래밍 복습, 코딩 테스트 준비", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = false, category = Category.STUDY),
+            TodoCard(title = "07/01 업무 계획", description = "프로젝트 미팅, 코드 리뷰", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = true, category = Category.WORK),
+            TodoCard(title = "07/01 약속", description = "오랜만의 친구와 저녁", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = false, category = Category.PROMISE),
+            TodoCard(title = "07/01 기타", description = "집 청소, 정리 정돈", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = true, category = Category.OTHER),
+            TodoCard(title = "07/02 운동", description = "유산소 운동, 스트레칭", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = false, category = Category.EXERCISE),
+            TodoCard(title = "07/02 학습", description = "데이터 사이언스 공부, 통계 복습", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = false, category = Category.STUDY),
+            TodoCard(title = "07/03 직장 업무", description = "클라이언트 회의, 코드 배포", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = true, category = Category.WORK),
+            TodoCard(title = "07/03 친구와의 약속", description = "카페에서 대화, 산책", user = DEFAULT_USER_LIST[1], comments = mutableListOf(), completed = false, category = Category.PROMISE),
+            TodoCard(title = "07/07 기타 할 일", description = "서류 정리, 물건 정리", user = DEFAULT_USER_LIST[0], comments = mutableListOf(), completed = true, category = Category.OTHER)
         ).apply {
-            val defaultComment1 = Comment(content = "Comment1 content", user = DEFAULT_USER2, todoCard = this[0])
-            val defaultComment2 = Comment(content = "Comment2 content", user = DEFAULT_USER1, todoCard = this[1])
-            this[0].comments.add(defaultComment1)
-            this[1].comments.add(defaultComment2)
+            val defaultCommentList = listOf(
+            Comment(content = "Comment1 content", user = DEFAULT_USER_LIST[1], todoCard = this[0]),
+            Comment(content = "Comment2 content", user = DEFAULT_USER_LIST[0], todoCard = this[1]))
+            this[0].comments.add(defaultCommentList[0])
+            this[1].comments.add(defaultCommentList[1])
         }
     }
 }
