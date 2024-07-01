fa548c39a3bed3503e9d0b04c618154e6acd4b6a
hongjinkwon <hjk0472@gmail.com>
2024-07-01 10:49:59 +0900

    :art: Pass Test on TodoControllerTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 
 }
 
--- src/test/kotlin/com/teamsparta/todosecurityproject/domain/todo/controller/TodoControllerTest.kt
+++ src/test/kotlin/com/teamsparta/todosecurityproject/domain/todo/controller/TodoControllerTest.kt
@@ -1,19 +1,17 @@
 package com.teamsparta.todosecurityproject.domain.todo.controller
 
 import com.ninjasquad.springmockk.MockkBean
+import com.teamsparta.todosecurityproject.common.exception.UnauthorizedException
 import com.teamsparta.todosecurityproject.domain.todo.dto.TodoCardResponse
 import com.teamsparta.todosecurityproject.domain.todo.dto.UpdateTodoCardRequest
 import com.teamsparta.todosecurityproject.domain.todo.model.Category
-import com.teamsparta.todosecurityproject.domain.todo.model.TodoCard
-import com.teamsparta.todosecurityproject.domain.todo.repository.TodoRepository
 import com.teamsparta.todosecurityproject.domain.todo.service.TodoService
-import com.teamsparta.todosecurityproject.domain.user.model.User
-import com.teamsparta.todosecurityproject.domain.user.repository.UserRepository
-import com.teamsparta.todosecurityproject.domain.user.service.UserService
+import com.teamsparta.todosecurityproject.domain.user.dto.UserResponse
 import com.teamsparta.todosecurityproject.infra.security.jwt.JwtPlugin
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.extension.ExtendWith
@@ -21,43 +19,37 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(MockKExtension::class)
+@ActiveProfiles("test")
 class TodoControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
     private val jwtPlugin: JwtPlugin,
+
     @MockkBean
-    private val todoService: TodoService,
-    @MockkBean
-    private val userService: UserService,
-    @MockkBean
-    private val userRepository: UserRepository,
-    @MockkBean
-    private val todoRepository: TodoRepository
+    private val todoService: TodoService
 ) : DescribeSpec({
     extension(SpringExtension)
+
+    afterContainer {
+        clearAllMocks()
+    }
 
     describe("PUT /todo/{todoCardId}는") {
         context("정상적으로 Update TodoCard 를 시도할 때") {
             it("200 status code 를 응답해야한다.") {
-                val user =
-                    userRepository.save(User(email = "test@naver.com", password = "password123", nickname = "testUser"))
-                val todoCard = todoRepository.save(
-                    TodoCard(title = "TestTitle", description = "TestDescription", category = Category.STUDY, user = user, completed = false)
-                )
-                val updateRequest = UpdateTodoCardRequest(title = "Update", description = "Updated Description", category = "EXERCISE", completed = true)
-                val updatedTodoCard = todoCard.apply {
-                    title = "Update"
-                    description = "Updated Description"
-                    category = Category.EXERCISE
-                    completed = true
-                }
-                val response = TodoCardResponse.from(updatedTodoCard)
-                every { todoService.updateTodoCard(user.id!!, todoCard.id!!, updateRequest) } returns response
+                val todoCardId = 1L
+                val updateRequest =
+                    """{"title":"update","description":"Updated Description","category":"EXERCISE","completed":"true"}"""
+                val userResponse = UserResponse(1L, "TestUser")
+                val todoCardResponse = TodoCardResponse("update","Updated Description", userResponse, Category.EXERCISE, true, null, null)
+
+                every { todoService.updateTodoCard(any<Long>(), any<Long>(), any<UpdateTodoCardRequest>())} returns todoCardResponse
 
                 val jwtToken = jwtPlugin.generateAccessToken(
                     subject = "1",
@@ -65,13 +57,39 @@ class TodoControllerTest @Autowired constructor(
                 )
 
                 val result = mockMvc.perform(
-                    put("/api/v2/todos/${todoCard.id}")
+                    put("/api/v2/todos/$todoCardId")
                         .header("Authorization", "Bearer $jwtToken")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
+                        .content(updateRequest)
                 ).andReturn()
 
                 result.response.status shouldBe 200
+            }
+        }
+
+        context("작성자가 아닌 TodoCard 의 Update 를 시도할 때") {
+            it("401 status code 를 응답해야한다.") {
+                val todoCardId = 1L
+                val updateRequest =
+                    """{"title":"update","description":"Updated Description","category":"EXERCISE","completed":"true"}"""
+
+                every { todoService.updateTodoCard(any<Long>(), any<Long>(), any<UpdateTodoCardRequest>())} throws UnauthorizedException("You do not have access.")
+
+                val jwtToken = jwtPlugin.generateAccessToken(
+                    subject = "1",
+                    email = "test@naver.com"
+                )
+
+                val result = mockMvc.perform(
+                    put("/api/v2/todos/$todoCardId")
+                        .header("Authorization", "Bearer $jwtToken")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(updateRequest)
+                ).andReturn()
+
+                result.response.status shouldBe 401
             }
         }
     }
